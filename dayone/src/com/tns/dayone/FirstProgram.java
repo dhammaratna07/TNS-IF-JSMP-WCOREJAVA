0af3efc656066f0fa6e1388364ac036cd30a2e68
dhammaratna <dhammaratna@localhost>
2025-08-01 13:22:05 +0530

    Day 1 update

--- dayone/src/com/tns/dayone/FirstProgram.java
+++ dayone/src/com/tns/dayone/FirstProgram.java
@@ -3,7 +3,44 @@ package com.tns.dayone;
 public class FirstProgram {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		//System.out.println("Hello World!");
+		/*Invalid variable declarations
+		 * 
+		 * int for=13; Keyword in 
+		 * int num 1=12; Sapce is not Allowed
+		 * 
+		 * int@num=12; Invaid @ not alloowed
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		/*
+		 * Valid declarations
+		 * 
+		 * int $num=12;
+		 */
+		
+		int $num=12;
+		System.out.println("Value of the number is"+$num);
+		String studentName="Rohit";
+		System.out.println("Name is :"+studentName);
+		
+		
+		
+		int rollNo=10;
+		System.out.print(rollNo);
+		byte seatNo=7;
+		System.out.print(seatNo);
+		float Sum=33/3f;
+		System.out.print(Sum);
+		
+		char ch=67;
+		System.out.print(ch);
+		
+		
+		
 	}
 
 }
